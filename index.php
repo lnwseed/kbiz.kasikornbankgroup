25ba10b94e82fb7b4bee5dcb87b7f8d74cc9e9b0
LION-HACK <33150334+lnwseed@users.noreply.github.com>
2021-12-15 23:28:16 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+    header('content-type: text/plain');
+    require 'Class_Kasikornbank.php';
+    $kbank = new kbiz_lnwseed(array(
+        'username' => '',
+        'password' => '',
+        'accountFrom' => '',
+    ));
+
+    //Step1
+     //$dataRsso = $kbank->login();
+     //$result = $kbank->validateSession($dataRsso);
+
+    //Step2
+     $res = $kbank->refreshSession();
+     //$res = $kbank->getTransactionHistory(""); //โชว์ประวัติการทำรายการต่างๆ
+     //$res = $kbank->getAccountSummaryList(); //โชว์ข้อมูลผู้ใช้ เช่น เลขบัญชี หรือ จำนวนเงินในบัญชี
+     print_r($res);
+
+?>
